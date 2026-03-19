c7db5339606f6ff5fd9e9e11ad683f9709d1b769
abhijeeth2004 <abhijeeth2004@gmail.com>
2026-03-19 14:49:12 +0530

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-# Find chromium executable
-CHROMIUM=$(which chromium || which chromium-browser || which google-chrome || find /nix -name "chromium" -type f 2>/dev/null | head -1)
-echo "Found Chromium at: $CHROMIUM"
-export PUPPETEER_EXECUTABLE_PATH="$CHROMIUM"
 node server.js
